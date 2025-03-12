10d7107655ba9f84ed6cf95801cf17826c6b3764
Abhinash Mishra <abhimishrabhi2580@gmail.com>
2025-03-12 20:21:50 +0530

    updatedProfile controller

--- BACKEND/controller/adminController.js
+++ BACKEND/controller/adminController.js
@@ -1,4 +1,3 @@
-
 const User = require("../models/userSchema");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
@@ -6,36 +5,36 @@ const bcrypt = require("bcrypt");
 module.exports.updateProfile = async (req, res) => {
   try {
     const { firstName, lastName, avatar, age, password } = req.body;
-    const allowedFields = ["firstName", "lastName", "avatar", "age", "password"];
+    const allowedFields = [
+      "firstName",
+      "lastName",
+      "avatar",
+      "age",
+      "password",
+    ];
     for (const field in req.body) {
       if (!allowedFields.includes(field)) {
         throw new Error("Field cannot be updated");
       }
     }
-    if (!firstName ||!lastName ||!avatar ||!age) {
-      throw new Error("Please provide all required fields");
+    const user = req.user;
+    if (firstName) {
+      user.firstName = req.body.firstName;
     }
-    if (password &&!validator.isStrongPassword(password)) {
-      throw new Error("Password is not strong enough");
+    if (lastName) {
+      user.lastName = req.body.lastName;
+    }
+    if (avatar) {
+      user.avatar = req.body.avatar;
+    }
+    if (age) {
+      user.age = req.body.age;
     }
     if (password) {
-      req.body.password = bcrypt.hashSync(password, 10);
+      user.password = bcrypt.hashSync(req.body.password, 10);
     }
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          avatar: req.body.avatar,
-          age: req.body.age,
-          password: req.body.password,
-        },
-      },
-      { new: true }
-    );
-    const { password: _,...rest } = updatedUser._doc;
-    res.status(200).json(rest);
+    await user.save();
+    res.status(200).json(user);
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: err.message });
